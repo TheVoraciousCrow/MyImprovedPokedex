b6456c07e0ca860e134bb0db2f797d8a2c7c5444
Nick D <ndiasio97@gmail.com>
2017-10-24 15:35:33 -0400

    Initialized basic structure

---
+++ src/Pokemon.java
@@ -0,0 +1,13 @@
+public class Pokemon {
+    private String species; //name
+    private int attack;
+    private int defense;
+    private int speed;
+    public Pokemon(){
+        attack = species.length() * 4 + 2;
+        defense = species.length() * 2 + 7;
+        speed = species.length() * 3 + 5;
+
+    }
+
+}
